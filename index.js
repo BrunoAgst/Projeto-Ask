489bccb38f6cb37adc059704fc104ea3160bf863
BrunoAgst <bruno_agst@hotmail.com>
2020-04-15 21:31:29 -0300

    create page perguntas.ejs and add bootstrap

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 
 // Estou dizendo para o express usar o EJS como view(rederizar o html)
 app.set("view engine","ejs");
+//definindo arquivos estáticos
+app.use(express.static('public')); 
 
 
 app.get("/",(req, res) => {
-    res.render("index")
+    res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
 });
 
 app.listen(3000, () =>{
