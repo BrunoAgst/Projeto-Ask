d28f21ee3b5af73217e11521c1ffdece2358fb34
BrunoAgst <bruno_agst@hotmail.com>
2020-04-16 00:03:23 -0300

    install sequelize and mysql2, configing forms to send data to the backend

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Estou dizendo para o express usar o EJS como view(rederizar o html)
 app.set("view engine","ejs");
 //definindo arquivos estáticos
-app.use(express.static('public')); 
+app.use(express.static('public'));
+//configurando o bodyparser 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 
 app.get("/",(req, res) => {
@@ -13,6 +18,13 @@ app.get("/",(req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    //pegando as informações do formulario com bodyparser
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("formulario recebido " + titulo + " " + descricao);
 });
 
 app.listen(3000, () =>{
